56cfc68f535b93f1a24cdcfbb971fbe66ef4879b
kajal-2011 <kajalrekha888@gmail.com>
2023-10-19 00:32:24 +0600

    blog added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,110 @@
 const Blog = ({ needFullHeight }) => {
   return (
-    <section className={`${needFullHeight && "min-h-100vh"} blog wrapper`}>
-      <h2 className="section-title">Comming soon......</h2>
+    <section className="blog-sec wrapper">
+      <h2 className="section-title " style={{ textAlign: "center" }}>
+        Immemorial trend blog
+      </h2>
+      <div className="blog-section">
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://i.pinimg.com/236x/b5/2b/99/b52b99ce024ae77bb52c4ad8f1c67dd8.jpg"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              childhood memories 90s, childhood memories
+            </h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://qph.cf2.quoracdn.net/main-qimg-778e66166ff811220491508a0ff3d899.webp"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              What's your favorite memory as a 90's kid{" "}
+            </h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://qph.cf2.quoracdn.net/main-qimg-ca8fb3660642864c74fc890427e5bce6-pjlq"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              Which are your best childhood memories being a 90s kid
+            </h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://i.pinimg.com/236x/bf/b1/6d/bfb16da41032cc2f699023d5b8f8c0dd.jpg"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              childhood memories 90s, childhood memories, childhood
+            </h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://i.pinimg.com/564x/05/7d/83/057d83d8c4aa9bb7b3487bb5e7662ad0.jpg"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              ChildHood memories 90s, Childhood memories
+            </h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://i.pinimg.com/originals/26/41/af/2641af80cdf1b42647faf386c0f58ec5.jpg"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">memories, childhood memories quotes</h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img src="https://i.pinimg.com/236x/d1/9b/f6/d19bf6e59164d52533f4aa679c0500ae.jpg" />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              childhood memories, childhood, childhood memories quotes
+            </h2>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog-img">
+            <img
+              src="https://tfe-bd.sgp1.cdn.digitaloceanspaces.com/uploads/1593101927.jpg"
+              alt="tank"
+            />
+          </div>
+          <div className="blog-overlay">
+            <h2 className="blog-title">
+              Changing nature of childhood thru time
+            </h2>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
